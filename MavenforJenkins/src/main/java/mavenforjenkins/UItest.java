4f2597a32121f691b0cd83280447b52af1bd1e95
KARTHICKRK28 <145431992+KARTHICKRK28@users.noreply.github.com>
2023-10-19 15:01:14 +0530

    Update UItest.java

--- MavenforJenkins/src/main/java/mavenforjenkins/UItest.java
+++ MavenforJenkins/src/main/java/mavenforjenkins/UItest.java
@@ -2,18 +2,29 @@ package mavenforjenkins;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.Assert;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
-
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class UItest {
 
+	@Parameters("Browser")
 	@Test
-	public void startBrowser() {
-		WebDriverManager.chromedriver().setup();
-		WebDriver driver= new ChromeDriver();
+	public void startBrowser(String BrowserName) {
+
+		System.out.println("Parameter value is :" + BrowserName);
+		WebDriver driver = null;
+		if (BrowserName.contains("Chrome")) {
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+		} else if (BrowserName.contains("Edge")) {
+			WebDriverManager.edgedriver().setup();
+			driver = new EdgeDriver();
+		}
+
 		driver.manage().window().maximize();
 		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
 		Assert.assertTrue(driver.getTitle().contains("Orange"));
